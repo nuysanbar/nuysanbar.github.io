e8209444d8b1b768942e4366681de65f7e11a9b1
Nuysanbar <102357504+nuysanbar@users.noreply.github.com>
2024-08-30 08:52:02 -0700

    Update modifiedProjectData.js

--- modifiedProjectData.js
+++ modifiedProjectData.js
@@ -3,7 +3,7 @@ const ModifiedData=[
         id:1,
         link:"https://www.youtube.com/embed/iCRd4o8b7kE",
         description:"Project Management System",
-        content:" software tool that helps plan, organize, and oversee projects. Key features include task management for assigning and tracking tasks, resource allocation, Gantt charts for visualizing timelines, collaboration tools for team communication, time tracking, budget management, reporting and analytics, and risk management. These features work together to ensure projects are completed on time, within budget, and with effective teamwork."
+        content:" software tool that helps plan, organize, and oversee projects. Key features include task management for assigning and tracking tasks, resource allocation, Gantt charts for visualizing timelines, collaboration tools for team communication, time tracking, budget management, reporting and analytics, and risk management."
     },
     {
         id:2,
