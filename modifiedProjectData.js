14741c448042a15a1bc921c6123164eba8eb56e3
Nuysanbar <102357504+nuysanbar@users.noreply.github.com>
2024-12-03 23:27:00 -0800

    Update modifiedProjectData.js

--- modifiedProjectData.js
+++ modifiedProjectData.js
@@ -8,7 +8,7 @@ const ModifiedData=[
     {
         id:2,
         link:"https://www.youtube.com/embed/iCRd4o8b7kE",
-        description:"Electronics store Management SAAS",
+        description:"Electronics Retailers Rival SAAS",
         content:" an e-commerce platform that enables electronic store businesses to create and manage online stores. It offers features such as product management, integrated payment processing, analytics, and 24/7 customer support. These features help businesses set up a professional online presence, streamline operations, and drive sales."
     },
     {
