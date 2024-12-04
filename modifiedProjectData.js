a5f5c9507841d9b09c7f23b414627fb0a5df3f5b
Nuysanbar <102357504+nuysanbar@users.noreply.github.com>
2024-12-04 00:14:30 -0800

    Update modifiedProjectData.js

--- modifiedProjectData.js
+++ modifiedProjectData.js
@@ -1,12 +1,12 @@
 const ModifiedData=[
     {
-        id:2,
+        id:1,
         link:"https://www.youtube.com/embed/iCRd4o8b7kE",
         description:"Electronics Retailers Rival SAAS",
         content:" an e-commerce platform that enables electronic store businesses to create and manage online stores. It offers features such as product management, integrated payment processing, analytics, and 24/7 customer support. These features help businesses set up a professional online presence, streamline operations, and drive sales."
     },
     {
-        id:1,
+        id:2,
         link:"https://www.youtube.com/embed/iCRd4o8b7kE",
         description:"Project Management System",
         content:" software tool that helps plan, organize, and oversee projects. Key features include task management for assigning and tracking tasks, resource allocation, Gantt charts for visualizing timelines, collaboration tools for team communication, time tracking, budget management, reporting and analytics, and risk management."
