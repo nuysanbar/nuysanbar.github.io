744695e6301112d3f0caacf17cad15eaa0a45c1c
Nuysanbar <102357504+nuysanbar@users.noreply.github.com>
2024-12-30 03:52:30 -0800

    Update modifiedProjectData.js

--- modifiedProjectData.js
+++ modifiedProjectData.js
@@ -1,19 +1,19 @@
 const ModifiedData=[
     {
         id:1,
-        link:"https://www.youtube.com/embed/iCRd4o8b7kE",
+        link:"https://github.com/nuysanbar/ERRP/tree/master",
         description:"Electronics Retailers Rival SAAS",
         content:" an e-commerce platform that enables electronic store businesses to create and manage online stores. It offers features such as product management, integrated payment processing, analytics, and 24/7 customer support. These features help businesses set up a professional online presence, streamline operations, and drive sales."
     },
     {
         id:2,
-        link:"https://www.youtube.com/embed/iCRd4o8b7kE",
+        link:"https://github.com/nuysanbar/ERRP/tree/master",
         description:"Project Management System",
         content:" software tool that helps plan, organize, and oversee projects. Key features include task management for assigning and tracking tasks, resource allocation, Gantt charts for visualizing timelines, collaboration tools for team communication, time tracking, budget management, reporting and analytics, and risk management."
     },
     {
         id:3,
-        link:"https://www.youtube.com/embed/iCRd4o8b7kE",
+        link:"https://github.com/nuysanbar/ERRP/tree/master",
         description:"Real time Stock Market line chart",
         content:"  a live line chart displays real-time stock price movements. It shows the stock's price over time, continuously updating to reflect the latest market data. This helps users track price trends and make timely investment decisions based on current market conditions."
     }
